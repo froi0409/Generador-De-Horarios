80838e15f2f3ced2769f5ca87dad741195c5df68
froi0409 <ferrubocana@gmail.com>
2023-08-26 13:03:46 -0600

    Se ha finalizado con la creación del proyecto con FLASK

--- FlaskApp/app/routes.py
+++ FlaskApp/app/routes.py
@@ -1,9 +1,21 @@
 from app import app
+from flask import request
 
 @app.route('/')
 def index():
     return 'Hola desde el backend'
 
-@app.route('/prueba')
+@app.route('/prueba', methods = ['POST'])
 def prueba():
     return 'Adios a todos'
+
+@app.route('/prueba/<variable>')
+def prueba_variable(variable):
+    return 'Hola' + variable
+
+@app.post('/pruebapost')
+def prueba_post():
+    message = request.form['message']
+    return 'prueba del post ' + message
+
+
