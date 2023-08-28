2a1ba4ef44518b413530ad59f5aaa5213a7b8051
froi0409 <ferrubocana@gmail.com>
2023-08-27 23:50:14 -0600

    Se ha terminado la configuración que permite la lectura de los archivos csv y su parseo a json

--- nodeapp/src/controllers/filesController.js
+++ nodeapp/src/controllers/filesController.js
@@ -1,5 +1,24 @@
+const csvFM = require('../services/csvFilesManagement/proccessFiles');
 
 const proccessCsvFiles =  async ( req, res ) => { 
+    const files = req.files; // Get files
+    const dataSets = [];
+
+    for (const file of files) {
+        const jsonFile = await csvFM.parseCsvToJson(file);
+        dataSets.push({
+            filename: file.originalname,
+            dataset: jsonFile
+        });
+    }
+
+    // dataSets.forEach(element => {
+    //     console.log(element.filename, ': ');
+    //     element.dataset.forEach(data => {
+    //         console.log('nombre: ', data.numero, ' - cant escritorios: ', data.cantidad_escritorios);
+    //     });
+    // });
+
     res.json({
         message: "Funcionó con éxito"
     });
--- nodeapp/src/routes/files.routes.js
+++ nodeapp/src/routes/files.routes.js
@@ -1,8 +1,13 @@
 const Router = require('express');
+const multer = require('multer');
 const filesController = require('../controllers/filesController');
 
 const router = Router();
+// Configuramos el almacenamiento de los archivos con multer
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
 
-router.post('/sendFiles', filesController.proccessCsvFiles);
+// ruta en la que recibimos n archivos
+router.post('/sendFiles', upload.array('files', 2), filesController.proccessCsvFiles);
 
 module.exports = router;
