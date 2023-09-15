82c052fe8632b6c37958c98a868d12e76e0ce1b2
froi0409 <ferrubocana@gmail.com>
2023-09-15 09:06:31 -0600

    Versión 2 de agregando profesores

--- nodeapp/src/services/priorities.js
+++ nodeapp/src/services/priorities.js
@@ -10,25 +10,27 @@ async function professorPriority(classSchedule, data) {
         let currentHour = '15:00';
         let i = 0;
         for (const currentData of professorsCoursess) {
+            
             // Si el profesor a evaluarse cambia, se reemplazará
-            if (currentData.id_professor !== currentProfessor) {
+            const idProfesor = Number(currentData.id_profesor)
+            const currentProfessorId = Number(currentProfessor);
+            if (idProfesor !== currentProfessorId) {
                 currentProfessor = currentData.id_profesor;
                 currentHour = '15:00';
                 i = 0;
-            }
-            
+            } 
             let count;
             do {
                 const queryValues = [currentData.id_profesor, classSchedule[i][0].startTime, classSchedule[i][0].endTime];
                 const inWorkTime = await conn.query('SELECT COUNT(*) AS no FROM Profesor WHERE id_profesor=? AND ? >= hora_entrada AND ? <= hora_salida', queryValues);
                 count = Number(inWorkTime[0].no);
-                console.log(queryValues);
-                console.log(count);
+                
                 if(count < 1) {
                     i++;
+                } else if (!isFree(idProfesor, classSchedule[i])) {
+                    count = 0;
                 }
             } while (count < 1);
-
             // Si el docente es títular de un curso, lo asignamos de una vez, porque al menos debería dar el curso en una sección
             if (currentData.titular === 1) {
                 // Recorre todos los salones en una hora en específico (ej. para ver qué salones están desocupados  las 13:00)
@@ -37,8 +39,6 @@ async function professorPriority(classSchedule, data) {
 
                     if (classSchedule[i][index].idProfessor === undefined) {
                         classSchedule[i][index].idProfessor = currentProfessor;
-                        console.log(classSchedule[i][index]);
-                        i++;
                         break;
                     }
                 }
@@ -57,8 +57,15 @@ async function putProfessors () {
     
 }
 
-function isOcuppied() {
-
+function isFree(idProfessor, hour) {
+    for (const slot of hour) {
+        const idP = Number(idProfessor);
+        const idPH = Number(hour.idProfesor);
+        if(idP === idPH) {
+            return false;
+        }
+    } 
+    return true;
 }
 
 module.exports = {
