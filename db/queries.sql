1538a4ca790f7fc0223cb9c7038903aff522dfc0
froi0409 <ferrubocana@gmail.com>
2023-09-17 21:11:10 -0600

    Se modificó la consulta que permite obtener las secciones, esto debido a que obtenia secciones duplicadas, lo cual afectaba el rendimiento

--- db/queries.sql
+++ db/queries.sql
@@ -1,5 +1,13 @@
 -- Obtención de Secciones
 SELECT m.nombre,m.duracion,m.numero_semestre,s.letra,s.cantidad_estudiantes,s.materia FROM Seccion AS s INNER JOIN Materia AS m ON s.materia=m.codigo WHERE s.Anio='2023' AND s.Semestre=1 ORDER BY m.numero_semestre;
+SELECT m.codigo AS codigo_curso,s.letra,m.nombre AS nombre_curso,m.numero_semestre,mc.carrera,s.cantidad_estudiantes FROM Seccion AS s INNER JOIN Materia AS m ON s.materia=m.codigo LEFT JOIN Materia_Carrera AS mc ON m.codigo=mc.materia ORDER BY carrera,numero_semestre;
+
+-- Obtención de Secciones, consulta mejorada
+SELECT m.codigo AS codigo_curso,s.letra,m.nombre AS nombre_curso,m.numero_semestre,mc.carrera,s.cantidad_estudiantes FROM Seccion AS s INNER JOIN Materia AS m ON s.materia=m.codigo LEFT JOIN Materia_Carrera AS mc ON m.codigo=mc.materia WHERE m.nombre='Matematica Basica 1' GROUP BY codigo_curso,letra ORDER BY carrera,numero_semestre;
+SELECT m.codigo AS codigo_curso,s.letra,m.nombre AS nombre_curso,m.numero_semestre,mc.carrera,s.cantidad_estudiantes FROM Seccion AS s INNER JOIN Materia AS m ON s.materia=m.codigo LEFT JOIN Materia_Carrera AS mc ON m.codigo=mc.materia GROUP BY codigo_curso,letra ORDER BY carrera,numero_semestre;
+
+
+
 
 -- Obtención de Profesores disponibles
 SELECT p.id_profesor,p.nombre AS nombre_profesor,p.apellido,p.hora_entrada,p.hora_salida,pm.titular,pm.materia,m.nombre AS nombre_materia,m.numero_semestre FROM Profesor AS p INNER JOIN Profesor_Materia AS pm ON p.id_profesor=pm.profesor INNER JOIN Materia AS m ON pm.materia=m.codigo;
--- nodeapp/src/services/priorities.js
+++ nodeapp/src/services/priorities.js
@@ -5,7 +5,7 @@ async function coursePriority(classSchedule) {
 
     try {
         // Get course by career
-        const sections = await conn.query('SELECT m.codigo AS codigo_curso,s.letra,m.nombre AS nombre_curso,m.numero_semestre,mc.carrera,s.cantidad_estudiantes FROM Seccion AS s INNER JOIN Materia AS m ON s.materia=m.codigo INNER JOIN Materia_Carrera AS mc ON m.codigo=mc.materia ORDER BY carrera,numero_semestre;');
+        const sections = await conn.query('SELECT m.codigo AS codigo_curso,s.letra,m.nombre AS nombre_curso,m.numero_semestre,mc.carrera,s.cantidad_estudiantes FROM Seccion AS s INNER JOIN Materia AS m ON s.materia=m.codigo LEFT JOIN Materia_Carrera AS mc ON m.codigo=mc.materia GROUP BY codigo_curso,letra ORDER BY carrera,numero_semestre;');
         
         // Get professor by course
         const professorsCourses = await conn.query('SELECT p.id_profesor,p.nombre AS nombre_profesor,p.apellido,p.hora_entrada,p.hora_salida,pm.titular,pm.materia,m.nombre AS nombre_materia,m.numero_semestre FROM Profesor AS p INNER JOIN Profesor_Materia AS pm ON p.id_profesor=pm.profesor INNER JOIN Materia AS m ON pm.materia=m.codigo');
@@ -21,72 +21,85 @@ async function coursePriority(classSchedule) {
 }
 
 async function putSections (classSchedule, sections, professorsCourses) {
-    for (const section of sections) {
+    const sectionsToRemove = [];
+    let cont = 0;
+    console.log(`Secciones: ${sections.length}`)
+    
+    for (let s = 0; s < sections.length; s++) {
+        const section = sections[s];
         for (let i = 0; i < classSchedule.length; i++) {
-            // Comprobamos que no haya alguna sección con un curso diferente de la misma carrera en el mismo horario
-            let overlap = false;
-            for (let j = 0; j < classSchedule[i].length; j++) {
-                const slot = classSchedule[i][j];
-                if (slot.courseId !== section.codigo_curso && slot.semesterNumber === section.numero_semestre && slot.career == section.carrera) {
-                    overlap = true;
-                    break;
-                }
-            }
-            if (overlap) break; // Para evitar hacer procesos de más
-            
-            //Si no traslapa, verificamos que exista algún docente que pueda dar el curso
-            let availableProfessor = false;
-            let selectedProfessor = {};
-            // Verificamos si el titular está disponible
-            let titularAvailable = false;
-            
-            for (const professor of professorsCourses) {
-                const idP = String(professor.id_profesor);
-                if (professor.materia === section.codigo_curso && professor.titular === 1) {
-                    if (isFree(professor.id_profesor, classSchedule[i])) {
-                        selectedProfessor.id_profesor = idP;
-                        selectedProfessor.first_name = professor.nombre_profesor;
-                        selectedProfessor.last_name = professor.apellido;
-                        availableProfessor = true;
+            // Si el valor de s ya se encuentra en sectionsToRemove, quiere decir que la sección ya fue ingresada al horario, por lo que no es necesario volver a analizarla
+            const iterator = sectionsToRemove.indexOf(s);
+            if (iterator === -1) {
+                // Comprobamos que no haya alguna sección con un curso diferente de la misma carrera en el mismo horario
+                let overlap = false;
+                for (let j = 0; j < classSchedule[i].length; j++) {
+                    const slot = classSchedule[i][j];
+                    if (slot.courseId !== section.codigo_curso && slot.semesterNumber === section.numero_semestre && slot.career == section.carrera) {
+                        overlap = true;
                         break;
                     }
                 }
-            }
-
-            // Si availableProfessor es falso, quiere decir que el profesor titular no está disponible, por lo que habrá que buscar a otro profesor
-            if (!availableProfessor) {
-
-            }
-
-            //Si no traslapa y hay docente disponible en el horario, lo colocamos en el salón disponible con el número más cercano a la cantidad de alumnos en la sección
-            let nearestCapacity = 5000;
-            let classroomPosition = 0;
-            for (let j = 0; j < classSchedule[i].length; j++) {
-                if (classSchedule[i][j].courseId === undefined) {
-                    const diff = classSchedule[i][j].classroomCapacity - section.cantidad_estudiantes;
-                    const diffAbs = Math.abs(diff);
-                    if (diffAbs < nearestCapacity) {
-                        nearestCapacity = diffAbs;
-                        // Si diff es menor que diffAbs, quiere decir que diff es negativo, por lo tanto, hacen falta estudiantes para llenar el salon, es mejor dejarlos en un salon donde sobren
-                        classroomPosition = j;
+                //if (overlap) break; // Para evitar hacer procesos de más
+                cont++;
+                console.log(cont)
+                
+                //Si no traslapa, verificamos que exista algún docente que pueda dar el curso
+                let availableProfessor = false;
+                let selectedProfessor = {};
+                // Verificamos si el titular está disponible
+                let titularAvailable = false;
+                
+                for (const professor of professorsCourses) {
+                    const idP = String(professor.id_profesor);
+                    if (professor.materia === section.codigo_curso && professor.titular === 1) {
+                        if (isFree(professor.id_profesor, classSchedule[i])) {
+                            selectedProfessor.id_profesor = idP;
+                            selectedProfessor.first_name = professor.nombre_profesor;
+                            selectedProfessor.last_name = professor.apellido;
+                            availableProfessor = true;
+                            break;
+                        }
                     }
                 }
-                
-            }
-            if (!overlap) {
-                classSchedule[i][classroomPosition].sectionLetter = section.letra;
-                classSchedule[i][classroomPosition].courseId = section.codigo_curso;
-                classSchedule[i][classroomPosition].courseName = section.nombre_curso;
-                classSchedule[i][classroomPosition].semesterNumber = section.numero_semestre;
-                classSchedule[i][classroomPosition].career = section.carrera;
-                classSchedule[i][classroomPosition].warningPriority = 0;
-                classSchedule[i][classroomPosition].professorId = selectedProfessor.id_profesor;
-                classSchedule[i][classroomPosition].professorFirstName = selectedProfessor.first_name;
-                classSchedule[i][classroomPosition].professorLastName = selectedProfessor.last_name;
 
+                // Si availableProfessor es falso, quiere decir que el profesor titular no está disponible, por lo que habrá que buscar a otro profesor
+                if (!availableProfessor) {
+
+                }
+
+                //Si no traslapa y hay docente disponible en el horario, lo colocamos en el salón disponible con el número más cercano a la cantidad de alumnos en la sección
+                let nearestCapacity = 5000;
+                let classroomPosition = 0;
+                for (let j = 0; j < classSchedule[i].length; j++) {
+                    if (classSchedule[i][j].courseId === undefined) {
+                        const diff = classSchedule[i][j].classroomCapacity - section.cantidad_estudiantes;
+                        const diffAbs = Math.abs(diff);
+                        if (diffAbs < nearestCapacity) {
+                            nearestCapacity = diffAbs;
+                            // Si diff es menor que diffAbs, quiere decir que diff es negativo, por lo tanto, hacen falta estudiantes para llenar el salon, es mejor dejarlos en un salon donde sobren
+                            classroomPosition = j;
+                        }
+                    }
+                    
+                }
+                if (!overlap) {
+                    classSchedule[i][classroomPosition].sectionLetter = section.letra;
+                    classSchedule[i][classroomPosition].courseId = section.codigo_curso;
+                    classSchedule[i][classroomPosition].courseName = section.nombre_curso;
+                    classSchedule[i][classroomPosition].semesterNumber = section.numero_semestre;
+                    classSchedule[i][classroomPosition].career = section.carrera;
+                    classSchedule[i][classroomPosition].warningPriority = 0;
+                    classSchedule[i][classroomPosition].professorId = selectedProfessor.id_profesor;
+                    classSchedule[i][classroomPosition].professorFirstName = selectedProfessor.first_name;
+                    classSchedule[i][classroomPosition].professorLastName = selectedProfessor.last_name;
+
+                    sectionsToRemove.push(s);
+                }
             }
         }
     }
+    console.log(sectionsToRemove);
     return classSchedule;
 }
 
