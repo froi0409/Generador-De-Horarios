c6b1b2e5914116775f0d437ae5c2da4c9fcb6eab
froi0409 <ferrubocana@gmail.com>
2023-09-18 00:37:39 -0600

    Se ha implementado un horario de clases visualmente atractivo para el usuario

--- front/scripts/generateClassSchedule.js
+++ front/scripts/generateClassSchedule.js
@@ -26,6 +26,30 @@ async function generarHorario() {
             const tbody = document.createElement("tbody");
 
             if (data.classSchedule && Array.isArray(data.classSchedule)) {
+                // Crear la primera fila con los nombres de los salones
+                const firstRow = tbody.insertRow();
+                firstRow.insertCell(); // Celda vacía para la esquina superior izquierda
+
+                // Obtener la lista de salones disponibles
+                const salones = new Set();
+                data.classSchedule.forEach(horario => {
+                    horario.forEach(clase => {
+                        if (clase.classroom) {
+                            salones.add(clase.classroom);
+                        }
+                    });
+                });
+
+                // Ordenar los salones numéricamente
+                const sortedSalones = [...salones].sort((a, b) => a - b);
+
+                // Agregar los nombres de los salones a la primera fila
+                sortedSalones.forEach(salon => {
+                    const cell = firstRow.insertCell();
+                    cell.textContent = `Salón ${salon}`;
+                    cell.style.border = "1px solid #ddd"; // Aplicar borde a las celdas de nombres de salones
+                });
+
                 // Iterar sobre los horarios y salones
                 data.classSchedule.forEach((horario, indexHorario) => {
                     const row = tbody.insertRow();
@@ -34,11 +58,14 @@ async function generarHorario() {
                         // Agregar el horario en la primera columna
                         const horaCell = row.insertCell();
                         horaCell.textContent = `${horario[0].startTime} - ${horario[0].endTime}`;
+                        horaCell.style.border = "1px solid #ddd"; // Aplicar borde a la celda de encabezado
 
                         // Iterar sobre los salones (a partir del segundo elemento de cada arreglo)
-                        for (let indexSalon = 1; indexSalon < horario.length; indexSalon++) {
-                            const clase = horario[indexSalon];
+                        for (let indexSalon = 0; indexSalon < sortedSalones.length; indexSalon++) {
+                            const salon = sortedSalones[indexSalon];
+                            const clase = horario.find(c => c.classroom === salon);
                             const cell = row.insertCell();
+                            cell.style.border = "1px solid #ddd"; // Aplicar borde a las celdas regulares
 
                             if (clase && clase.courseName) {
                                 cell.textContent = `${clase.courseName}\n${clase.professorFirstName} ${clase.professorLastName}`;
